73a50bb9b2297c670b73f1e39201c6036de57835
Alonso Parra <alonsogparra@gmail.com>
2019-11-17 17:26:59 -0400

    Navbar updated with sidenav

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import SignedInLinks from './SignedInLinks';
+import SignedInLinksSidenav from './SignedInLinksSidenav';
+import SignedOutLinksSidenav from './SignedOutLinksSidenav';
 import SignOutLinks from './SignedOutLinks';
 
 const navbar = (props) => {
   return (
     <div>
+
       <nav className="nav-wrapper grey darken-3">
         <div className="container">
           <Link to='/' className="brand-logo">Mario Bro's Plans</Link>
-          <ul className="right">
+          <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+          <div className="right hide-on-med-and-down">
             <SignedInLinks />
             <SignOutLinks />
-          </ul>
+          </div>
         </div>
       </nav>
+
+      <SignedInLinksSidenav />
+      <SignedOutLinksSidenav />
+
     </div>
   );
 }
