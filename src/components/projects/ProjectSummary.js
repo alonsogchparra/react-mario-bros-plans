8d54d85939a840575e8dcc59ccedb8d4e0c2fe9b
Alonso Parra <alonsogparra@gmail.com>
2019-11-18 14:41:06 -0400

    ProjectSummary component built

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const projectSummary = ({project}) => {
+  return (
+    <div className="card z-depth-0 project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <div className="card-title">Project</div>
+        <p>Posted by Fulanito</p>
+        <div className="grey-text">03/03/12</div>
+      </div>
+    </div>
+  )
+}
+
+export default projectSummary
