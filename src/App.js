a6aa7c24bea5b5072ca13aab793b162afa43a873
Alonso Parra <alonsogparra@gmail.com>
2019-11-26 17:02:37 -0400

    Adding new routes on App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProject from './components/projects/CreateProject';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path="/" component={Dashboard} />
+        <Route path="/create" component={CreateProject} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
       </Switch>
     </BrowserRouter>
   );
