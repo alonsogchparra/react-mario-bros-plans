e7d60b45b3fc1922a02bfdd2fa68c4b2c391333d
Alonso Parra <alonsogparra@gmail.com>
2019-12-02 16:19:16 -0400

    Updating index with react-redux, redux, firebase, firestorem thunk elements

--- src/index.js
+++ src/index.js
@@ -1,10 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { createFirestoreInstance, getFirestore, reduxFirestore } from 'redux-firestore';
+import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 import './index.css';
 import App from './App';
+import rootReducer from './store/reducers';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//  The config info you will get it
+//  when you create a project on console.firebase.google.com
+const config = {
+  apiKey: "XXXX",
+  authDomain: "XXXXX",
+  databaseURL: "XXXXX",
+  projectId: "XXXXX",
+  storageBucket: "XXXX",
+  messagingSenderId: "XXXXX",
+  appId: "XXXXX",
+  measurementId: "XXXXXX"
+};
+firebase.initializeApp(config);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer,
+  composeEnhancers(
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+    reduxFirestore(firebase)
+  ));
+
+  const rrfConfig = {
+    userProfile: 'users',
+    useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+  }
+
+  const rrfProps = {
+    firebase,
+    config: rrfConfig,
+    dispatch: store.dispatch,
+    createFirestoreInstance // <- needed if using firestore
+  }
+
+  const app = (
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        <App />
+      </ReactReduxFirebaseProvider>
+    </Provider>
+  )
+
+ReactDOM.render(app, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
