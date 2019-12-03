0f2cbe98368ddd838e12fbc393c707fca50c277a
Alonso Parra <alonsogparra@gmail.com>
2019-12-03 16:22:46 -0400

    ProjectDetails updated with react-redux

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
 
-const projectDetails = () => {
+
+const projectDetails = ({project}) => {
   return (
     <div>
       <div className="container section project-details">
         <div className="card z-depth-0">
           <div className="card-content">
-            <span className="card-title">Project Title</span>
-            <p>Project Content</p>
+            <span className="card-title">{project.title}</span>
+            <p>{project.content}</p>
           </div>
           <div className="card-action grey lighten-4 grey-text">
             <div>Post by FullName of the author</div>
@@ -19,4 +21,16 @@ const projectDetails = () => {
   )
 }
 
-export default projectDetails
+const mapStateToProps = (state, ownProps) => {
+  console.log('state', state);
+  console.log('ownProps', ownProps);
+  const id = ownProps.match.params.id;
+  const projects = state.projects.projects;
+  const project = projects ? projects[id] : null;
+
+  return {
+    project: project
+  }
+}
+
+export default connect(mapStateToProps)(projectDetails)
