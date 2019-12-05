3be63e269e962aa9a351dca43f2a31e042541575
Alonso Parra <alonsogparra@gmail.com>
2019-12-05 16:48:18 -0400

    SignUp component updated with redux and methods to connect with the signup action

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import * as actions from '../../store/actions';
 
 class SignUp extends Component {
 
@@ -9,32 +12,63 @@ class SignUp extends Component {
     lastName: ''
   }
 
+  changeHandler = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault();
+    this.props.onSignUp(this.state);
+  }
+
   render () {
+    const { auth, authError } = this.props;
+    if (auth.uid) return <Redirect to="/" />
     return (
       <div className="container">
-        <form onSubmit className="white">
+        <form onSubmit={this.submitHandler} className="white">
           <h5 className="grey-text text-darken-3">Sign Up</h5>
           <div className="input-field">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" />
+            <input type="email" name="email" id="email" onChange={this.changeHandler} />
           </div>
           <div className="input-field">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" />
+            <input type="password" name="password" id="password" onChange={this.changeHandler} />
           </div>
           <div className="input-field">
             <label htmlFor="firstname">First Name</label>
-            <input type="text" name="firstname" id="firstname" />
+            <input type="text" name="firstName" id="firstName" onChange={this.changeHandler} />
           </div>
           <div className="input-field">
             <label htmlFor="lastname">Last Name</label>
-            <input type="text" name="lastname" id="lastname" />
+            <input type="text" name="lastName" id="lastName" onChange={this.changeHandler} />
           </div>
-          <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
+          <div className="input-field">
+            <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
+          </div>
+          <div className="red-text center">
+            { authError ? <p>{ authError }</p> : null }
+          </div>
         </form>
       </div>
     )
   }
 }
 
-export default SignUp
+const mapStateToProps = state => {
+  return {
+    auth: state.firebase.auth,
+    authError: state.auth.authError
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onSignUp: (newUser) => dispatch(actions.signUp(newUser))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp)
