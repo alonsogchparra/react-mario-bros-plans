3bb14ed8ba6c643e78bf0a5b1233f8ae5f42507d
Alonso Parra <alonsogparra@gmail.com>
2019-12-05 16:50:02 -0400

    SignedInLinks updated with redux

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../../store/actions';
+
 
 const signedInLinks = (props) => {
   return (
     <ul className="right">
       <li><NavLink to="/create">New Project</NavLink></li>
-      <li><a>Log Out</a></li>
+      <li><a onClick={props.onSignOut}>Log Out</a></li>
       <li><NavLink to="/" className="btn btn-floating pink lighten-1">AP</NavLink></li>
     </ul>
   );
 }
 
-export default signedInLinks;
+const mapDispatchToProps = dispatch => {
+  return {
+    onSignOut: () => dispatch(actions.signOut())
+  }
+}
+
+export default connect(null, mapDispatchToProps)(signedInLinks);
