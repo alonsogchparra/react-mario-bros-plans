74bd7bc55413a5e9d9ede34baa81d1e35b5cbd02
Alonso Parra <alonsogparra@gmail.com>
2019-12-06 13:28:22 -0400

    Navbar updated with redux and adding the elements for sidenav

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import { connect } from 'react-redux'
+import { isLoaded } from 'react-redux-firebase';
 import SignedInLinks from './SignedInLinks';
-import SignedInLinksSidenav from './SignedInLinksSidenav';
-import SignedOutLinksSidenav from './SignedOutLinksSidenav';
 import SignOutLinks from './SignedOutLinks';
 
-const navbar = (props) => {
+class Navbar extends Component {
+
+  render() {
+    const { auth, profile } = this.props;
+    const links = auth.uid ? <SignedInLinks profile={profile} /> : <SignOutLinks />;
+    const checkIsLoaded = isLoaded(auth) ? links : '';
+    const sideNavLinks = auth.uid ? (
+      <div>
+        <li className="sidenav-close"><NavLink to="/create">New Project</NavLink></li>
+        <li className="sidenav-close"><a>Log Out</a></li>
+      </div>
+    ) : (
+      <div>
+        <li className="sidenav-close"><NavLink to="/signin">Sign In</NavLink></li>
+        <li className="sidenav-close"><NavLink to="/signup">Sign Up</NavLink></li>
+      </div>
+    );
+    const sideNavCheckIsLoaded = isLoaded(auth) ? sideNavLinks : '';
+
   return (
     <div>
 
@@ -14,17 +32,25 @@ const navbar = (props) => {
           <Link to='/' className="brand-logo">Mario Bro's Plans</Link>
           <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
           <div className="right hide-on-med-and-down">
-            <SignedInLinks />
-            <SignOutLinks />
+            { checkIsLoaded }
           </div>
         </div>
       </nav>
 
-      <SignedInLinksSidenav />
-      <SignedOutLinksSidenav />
+      <ul className="sidenav" id="mobile-demo">
+        {sideNavCheckIsLoaded}
+      </ul>
 
     </div>
   );
 }
+}
 
-export default navbar;
+const mapStateToProps = state => {
+  return {
+    auth: state.firebase.auth,
+    profile: state.firebase.profile
+  }
+}
+
+export default connect(mapStateToProps)(Navbar);
