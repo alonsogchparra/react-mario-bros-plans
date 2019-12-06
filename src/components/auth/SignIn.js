8d40e9ab0849a2de4cf48e5d3e6cd2c00904dec2
Alonso Parra <alonsogparra@gmail.com>
2019-12-05 22:32:21 -0400

    SignIn component updated with redux

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,4 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { isLoaded } from 'react-redux-firebase';
+import * as actions from '../../store/actions';
+
 
 class SignIn extends Component {
 
@@ -15,12 +20,15 @@ class SignIn extends Component {
 
   submitHandler = (e) => {
     e.preventDefault();
+    this.props.onSignIn(this.state);
   }
 
   render () {
-    console.log('SignIn', this.state);
+    const { auth, authError } = this.props;
+    if (auth.uid) return <Redirect to="/" />
     return (
       <div className="container">
+        {isLoaded(auth) ? (
         <form onSubmit={this.submitHandler} className="white">
           <h5 className="grey-text text-darken-3">Sign In</h5>
           <div className="input-field">
@@ -33,11 +41,28 @@ class SignIn extends Component {
           </div>
           <div className="input-field">
             <button className="btn pink lighten-1 z-depth-0">Login</button>
+            <div className="red-text center">
+              { authError ? <p>{authError}</p> : null }
+            </div>
           </div>
         </form>
+        ) : '' }
       </div>
     )
   }
 }
 
-export default SignIn
+const mapStateToProps = state => {
+  return {
+    auth: state.firebase.auth,
+    authError: state.auth.authError
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onSignIn: (credentials) => dispatch(actions.signIn(credentials))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn)
