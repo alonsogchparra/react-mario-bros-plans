d8805d6c96839b0a001965b488d5615e0bb78789
Alonso Parra <alonsogparra@gmail.com>
2019-12-05 22:22:07 -0400

    action creator created signin

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,22 @@
 import * as actionTypes from './actionTypes';
 
+export const signIn = (credentials) => {
+  return (dispatch, getState, {getFirebase}) => {
+    const firebase = getFirebase();
+
+    firebase.auth().signInWithEmailAndPassword(
+      credentials.email,
+      credentials.password
+    )
+    .then(() => {
+      dispatch({ type: actionTypes.LOGIN_SUCCESS });
+    })
+    .catch((err) => {
+      dispatch({ type: actionTypes.LOGIN_ERROR, err });
+    })
+  }
+}
+
 export const signOut = () => {
   return (dispatch, getState, {getFirebase}) => {
     const firebase = getFirebase();
