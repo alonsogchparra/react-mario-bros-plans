a6baf1dedfc899887f78395e8a7bd06980ee0809
Alonso Parra <alonsogparra@gmail.com>
2019-12-06 22:49:58 -0400

    ProjectSummary updated with props to show info from db

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment';
 
 const projectSummary = ({project}) => {
   return (
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
         <div className="card-title">{project.title}</div>
-        <p>Posted by Fulanito</p>
-        <div className="grey-text">03/03/12</div>
+        <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
+        <div className="grey-text">{moment(project.createdAt.toDate()).calendar()}</div>
       </div>
     </div>
   )
